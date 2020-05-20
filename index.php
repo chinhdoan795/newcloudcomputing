01523385d92b4bea51049f3e4ab6f96ce5175e62
chinhdoan795 <52903177+chinhdoan795@users.noreply.github.com>
2020-05-20 10:56:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <title></title>
+</head>
 <body>
-	<h1>My Frist Page PHP</h1>
-	<?php
-	echo "Wednesday !!!@@@";
-	echo "Cloud computing";
-	echo "change in fixbug branch";
-	?>
+<br><br>
+<center>
+    <!-- nếu form có upload ảnh thì cần chèn thuộc tính 'enctype' -->
+    <form action="" method="post" enctype="multipart/form-data">
+        <fieldset style="background-color: #FFFFCC">
+            <legend>Profile</legend>
+            Name: <input type="text" name="name" required maxlength="50" size="33">
+            Product Image:
+            <!-- accept="image/*" : chỉ cho phép upload file ảnh -->
+            <input type="file" name="image" accept="images/*" required>
+            <br><br>
+            Facebook: <input type="text" name="facebook" required maxlength="250" size="33">
+            <br><br>
+            <input type="submit" value="ADD" name="add">
+        </fieldset>
+    </form>
+</center>
 
 </body>
 </html>
